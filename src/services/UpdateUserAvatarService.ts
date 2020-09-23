0438c0d05fec4866610e42cfebcea41a63f7c2fb
Airton Brasil <airton.brasil@eduzz.com>
2020-09-23 19:37:52 -0300

    Adição de rota para atualizar avatar de usuario

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+    user_id: string;
+    avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilepath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExist = await fs.promises.stat(userAvatarFilepath);
+      if (userAvatarFileExist) {
+        await fs.promises.unlink(userAvatarFilepath);
+      }
+    }
+    user.avatar = avatarFilename;
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
